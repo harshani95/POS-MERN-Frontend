689ad9a940377877a79b1c2fbbbafcd02232660a
harshani <ruwanthikapemarathna@gmail.com>
2024-02-06 12:45:23 +0530

    order component updated

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Customer from "./Customer.tsx";
+import AxiosInstance from '../config/axiosInstance.ts';
+import Product from "./Product.tsx";
 
-function Order() {
+interface Cart{
+    _id:string | undefined,
+    description:string| undefined,
+    unitPrice:number| '',
+    qty:number| undefined,
+    total:number| undefined
+}
+
+const Order:React.FC = ()=>{
     const styleObj:React.CSSProperties={
         marginBottom:'20px'
     }
@@ -15,6 +26,60 @@ function Order() {
         margin:'0'
     }
 
+    const [customers, setCustomers]=useState<Customer[]>([]);
+    const [products, setProducts]=useState<Product[]>([]);
+    const [cart, setCart]=useState<Cart[]>([]);
+
+    const [address,setAddress]=useState('');
+    const [salary,setSalary]=useState<number | ''>('');
+
+    const [selectedCustomer,setSelectedCustomer] = useState<Customer | null>(null);
+    const [selectedProduct,setSelectedProduct] = useState<Product | null>(null);
+
+    const [name,setName]=useState('');
+    const [userQty,setUserQty]=useState<number>(0);
+    const [description,setDescription]=useState('');
+    const [unitPrice,setUnitPrice]=useState<number | ''>('');
+    const [qtyOnHand,setQtyOnHand]=useState<number | ''>('');
+    const [netTotal,setNetTotal]=useState<number>(0);
+
+
+    useEffect(()=>{
+        findAllCustomers();
+        findAllProducts();
+    }, []);
+
+    const findAllCustomers= async ()=>{
+        const response = await AxiosInstance.get('/customers/find-all?searchText=&page=1&size=10');
+        setCustomers(response.data);
+    }
+
+    const findAllProducts= async ()=>{
+        const response = await AxiosInstance.get('/products/find-all?searchText=&page=1&size=10');
+        setProducts(response.data);
+    }
+
+    const getCustomerById= async (id:string)=>{
+        const customer = await AxiosInstance.get('/customers/find-by-id/'+id);
+        setSelectedCustomer(customer.data);
+        setAddress(customer.data.address)
+        setSalary(parseFloat(customer.data.salary))
+    }
+
+    const getProductById= async (id:string)=>{
+        const product = await AxiosInstance.get('/products/find-by-id/'+id);
+        setSelectedProduct(product.data);
+        setName(product.data.name);
+        setDescription(product.data.description);
+        setQtyOnHand(product.data.qtyOnHand);
+        setUnitPrice(product.data.unitPrice);
+
+    }
+
+    const addToCart= async (newItem:Cart)=>{
+        setCart((prevState)=>[...prevState,newItem]);
+    }
+
     return (
         <>
             <br/>
@@ -24,73 +89,100 @@ function Order() {
                     <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="customer">Select Customer</label>
-                            <select id="customer" className='form-control'>
-                                <option value="Use Options" disabled defaultValue='Use Options'>Use Options</option>
-                                <option value="#">Customer 1</option>
-                                <option value="#">Customer 2</option>
+                            <select id="customer" className='form-control' onChange={(e)=>{
+                                getCustomerById(e.target.value)
+                            }}>
+                                <option value="">Select Value</option>
+                                {customers.map((customer, index)=>(
+                                    <option key={index} value={customer._id}>{customer.name}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
-                    <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
+
+                    {/* <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="name">Customer Name</label>
                             <input disabled type="text" className='form-control' id='name'/>
                         </div>
-                    </div>
+                    </div> */}
+
                     <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="address">Customer Address</label>
-                            <input disabled type="text" className='form-control' id='address'/>
+                            <input value={address} disabled type="text" className='form-control' id='address'/>
                         </div>
                     </div>
+
                     <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
                         <div className="form-group">
-                            <label htmlFor="salary">Customer Address</label>
-                            <input disabled type="number" className='form-control' id='salary'/>
+                            <label htmlFor="salary">Customer Salary</label>
+                            <input value={salary} disabled type="number" className='form-control' id='salary'/>
                         </div>
                     </div>
                 </div>
                 <hr/>
+
                 <div className="row">
                     <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="product">Select Product</label>
-                            <select id="product" className='form-control'>
-                                <option value="Use Options" disabled defaultValue='Use Options'>Use Options</option>
-                                <option value="#">Customer 1</option>
-                                <option value="#">Customer 2</option>
+                            <select id="product" className='form-control' onChange={(e)=>{
+                                getProductById(e.target.value)
+                            }}>
+                                <option value="">Select Value</option>
+                                {products.map((product, index)=>(
+                                    <option key={index} value={product._id}>{product.name}</option>
+                                ))}
+
+                                
                             </select>
                         </div>
                     </div>
+
                     <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="description">Product Description</label>
-                            <input type="text" disabled className='form-control' id='description'/>
+                            <input value={description} type="text" disabled className='form-control' id='description'/>
                         </div>
                     </div>
+
                     <div className="col-12 col-sm-6 col-md-2" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="price">Unit Price</label>
-                            <input type="number" disabled className='form-control' id='price'/>
+                            <input value={unitPrice}  type="number" disabled className='form-control' id='price'/>
                         </div>
                     </div>
+
                     <div className="col-12 col-sm-6 col-md-2" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="qtyOnHand">QTY On Hand</label>
-                            <input type="number" disabled className='form-control' id='qtyOnHand'/>
+                            <input value={qtyOnHand} type="number" disabled className='form-control' id='qtyOnHand'/>
                         </div>
                     </div>
+
                     <div className="col-12 col-sm-6 col-md-2" style={styleObj}>
                         <div className="form-group">
                             <label htmlFor="qty">QTY</label>
-                            <input type="number" className='form-control' id='qty'/>
+                            <input onChange={(e)=>{setUserQty(parseFloat(e.target.value))}}  type="number" className='form-control' id='qty'/>
                         </div>
                     </div>
                 </div>
                 <hr/>
                 <div className="row">
                     <div className="col-12">
-                        <button className='btn btn-primary col-12'>+ Add Product</button>
+                        <button className='btn btn-primary col-12'
+                        onClick={()=>{
+
+                            const cartProduct:Cart= {
+                                _id:selectedProduct._id,
+                                description:description,
+                                unitPrice:unitPrice,
+                                qty:userQty,
+                                total:(userQty*(unitPrice?unitPrice:0))
+                            }
+                            addToCart(cartProduct);
+                        }}>+ Add Product</button>
                     </div>
                 </div>
 
@@ -101,7 +193,7 @@ function Order() {
                         <table className='table table-hover table-bordered'>
                             <thead>
                             <tr>
-                                <th>#Id</th>
+                                <th>#ID</th>
                                 <th>Product Name</th>
                                 <th>Unit Price</th>
                                 <th>QTY</th>
@@ -110,17 +202,19 @@ function Order() {
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>#1001</td>
-                                <td>Product 1</td>
-                                <td>240.00</td>
-                                <td>20</td>
-                                <td>25000.00</td>
+                            {cart.map((data, index)=>(
+                            <tr key={index}>
+                                 <td>#{data._id}</td>
+                                    <td>{data.description}</td>
+                                    <td>{data.unitPrice}</td>
+                                    <td>{data.qty}</td>
+                                    <td>{data.total}</td>
                                 <td>
-                                    <button className='btn btn-outline-danger btn-sm'>Remove</button>
+                                    <button onClick={(e)=>{setCart((prevState)=>prevState.filter((cartData)=>cartData._id!==data._id));
+                                    }}className='btn btn-outline-danger btn-sm'>Remove</button>
                                 </td>
                             </tr>
-
+                             ))}
                             </tbody>
                         </table>
 
@@ -129,11 +223,17 @@ function Order() {
                         <div className="bottom-context" style={bottomContext}>
                             <div className="total-outer">
                                 <h1 style={totalText}>
-                                    Total : 2550.00
+                                    Total : {netTotal}
                                 </h1>
                             </div>
                             <div className="place-order-button-context">
-                                <button className='btn btn-primary'>Place Order</button>
+                                <button className='btn btn-primary'onClick={async ()=>{await AxiosInstance.post('/orders/create/',{
+                                    date:new Date(),
+                                    customerDetails:selectedCustomer,
+                                    totalCost:130,
+                                    products:cart
+                                    });
+                                    }}>Place Order</button>
                             </div>
                         </div>
 
