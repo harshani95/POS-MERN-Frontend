bb62c4fdef2259429e2c977a5044873f9407407d
harshani <ruwanthikapemarathna@gmail.com>
2024-02-19 11:23:36 +0530

    updated minQtyCard

--- src/components/cards/MinQtyCard.tsx
+++ src/components/cards/MinQtyCard.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-function MinQtyCard() {
+interface ProductProps {
+    image:string,
+    description:string,
+    name:string
+}
+
+function MinQtyCard(props:ProductProps) {
 
     const styleObj:React.CSSProperties={
         width:'100%',
@@ -9,12 +15,11 @@ function MinQtyCard() {
 
     return(
         <div className="card" style={styleObj}>
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
+            <img src={props.image} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text">{props.description}</p>
+                </div>
         </div>
     )
 }
