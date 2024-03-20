2d2a19584c24c24e251d137cc5b457ed602358f3
harshani <ruwanthikapemarathna@gmail.com>
2024-03-20 19:30:36 +0530

    added some css styles to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -44,8 +44,8 @@ const  Home:React.FC = ()=>{
                 <div className="row">
                     <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                         <DefaultCard
-                            thumbnail='https://img.freepik.com/free-vector/collection-people-carrying-shopping-bags_23-2148209436.jpg?size=626&ext=jpg&ga=GA1.2.1010534045.1685872185&semt=sph'
-                            description='This is a wider card with'
+                            thumbnail='https://img.freepik.com/free-photo/female-friends-out-shopping-together_53876-25041.jpg?t=st=1710940189~exp=1710943789~hmac=cbf1de7ce40cd180617a77f8b755cc8c15c9833689488248f32a408581e661c2&w=740'
+                            description='All Customers in System'
                             title='Customers'
                             value={customerCount}
                             key={1}
@@ -54,7 +54,7 @@ const  Home:React.FC = ()=>{
                     <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                         <DefaultCard
                             thumbnail='https://img.freepik.com/free-vector/t-shirt-print-demand-services-promotional-apparel-design-merch-clothing-custom-merchandise-products-merch-design-service-concept_335657-124.jpg?size=626&ext=jpg&ga=GA1.2.1010534045.1685872185&semt=sph'
-                            description='This is a wider card with '
+                            description='All Products in System '
                             title='Products'
                             value={productCount}
                             key={1}
@@ -63,7 +63,7 @@ const  Home:React.FC = ()=>{
                     <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                         <DefaultCard
                             thumbnail='https://img.freepik.com/premium-photo/black-friday-composition-with-three-bags-cart_23-2147709333.jpg?w=740'
-                            description='This is a wider card with supporting'
+                            description='All Orders in System '
                             title='Orders'
                             value={orderCount}
                             key={1}
@@ -72,7 +72,7 @@ const  Home:React.FC = ()=>{
                     <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                         <DefaultCard
                             thumbnail='https://img.freepik.com/free-photo/person-carrying-lot-cash_53876-65367.jpg?w=740&t=st=1702482890~exp=1702483490~hmac=2f80e70d6f5d8949c51877db9816a9fae2fcb07d8a7d3bbec21df8acde8e1be2'
-                            description='This is a wider card with supporting text below.'
+                            description='All Income in System '
                             title='Income'
                             value={income}
                             key={1}
